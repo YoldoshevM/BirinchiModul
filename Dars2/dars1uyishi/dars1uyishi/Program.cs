4be67040b97b060d842ab54f08c84e2f4ebe1256
YoldoshevM <yoldoshevm396@gmail.com>
2025-12-03 23:11:53 +0500

    1chidarsuyishi

---
+++ Dars2/dars1uyishi/dars1uyishi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dars1uyishi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
