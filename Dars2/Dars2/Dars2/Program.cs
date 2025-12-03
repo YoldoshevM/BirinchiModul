e5800547ea3253a3b74c36aa72510b546e74e8da
YoldoshevM <yoldoshevm396@gmail.com>
2025-12-03 23:04:56 +0500

    1chidarsuyishi

--- Dars2/Dars2/Dars2/Program.cs
+++ Dars2/Dars2/Dars2/Program.cs
@@ -1,10 +1,9 @@
-﻿namespace Dars2
+﻿namespace Dars2;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
