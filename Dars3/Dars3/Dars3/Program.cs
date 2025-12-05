fa2ab36aa65d8ce22ea1680cb7fc02d44cbd364b
YoldoshevM <yoldoshevm396@gmail.com>
2025-12-05 23:44:06 +0500

    2 chi fayl tugadi

--- Dars3/Dars3/Dars3/Program.cs
+++ Dars3/Dars3/Dars3/Program.cs
@@ -7,7 +7,7 @@ internal class Program
     static void Main(string[] args)
     {
 
-        //     Fayl 1
+        //                 FAYL 1.3.1
 
 
 
@@ -534,7 +534,563 @@ internal class Program
 
 
 
-        //     Fayl 2
+        //                 FAYL 1.3.3
+
+
+
+        // 1
+
+
+        //for ( int i = 14; i <= 20; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+
+        // 2
+
+        //for (int i = 11; i <= 99; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        // 3
+
+        //for (int i = 12; i <= 200; i++)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        // 4
+
+        //for (int i = 25; i >= 10; i--)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        // 5
+
+        //for (int i = 500; i >= 80; i--)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+        // 6
+
+        //for (int i = 11 ; i <= 88 ; i++)
+        //{
+        //    if ( i % 2 != 0)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}   
+
+        // 7
+
+        //for (int i = 12 ; i <= 25 ; i++)
+        //{
+        //    if ( i % 2 == 0)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}   
+
+        // 8
+
+        //for (int i = 15 ; i <= 88 ; i++)
+        //{
+        //    if ( i % 2 != 0)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}   
+
+        // 9
+
+        //for (int i = 99 ; i >= 10 ; i--)
+        //{
+        //    if ( i % 2 == 0)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}   
+
+
+        // 10
+
+        //Console.Write(" a sonini kiriting : "); 
+
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : "); 
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+        //for ( int i = firstIntValue; i <= secondIntValue; i++  )
+        //{
+        //    if ( i % 3 == 0 )
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+
+        // 11
+
+        //Console.Write(" a sonini kiriting : "); 
+
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+        //for ( int i = firstIntValue; i <= secondIntValue; i++ )
+        //{
+        //    if ( i % 5 == 0 && i % 6 == 0 )
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+
+        // 12
+
+        //Console.Write(" a sonini kiriting : "); 
+
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+        //var counter = 0;
+
+        //for ( int i = firstIntValue; i <= secondIntValue; i++ )
+        //{
+        //    if ( i % 10 == 0 )
+        //    {
+        //        counter++;
+        //    }
+        //}
+
+        //Console.WriteLine(counter);
+
+
+        // 13
+
+
+        //Console.Write(" a sonini kiriting : "); 
+
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+        //for ( int i = secondIntValue; i >= firstIntValue; i--)
+        //{
+        //    if ( i % 7 == 0 || i % 8 == 0 )
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+
+        // 14
+
+        //var yigindi = 0;
+
+        //for (int i = 1; i <= 100; i++)
+        //{
+        //    if ( i % 2 == 0 )
+        //    {
+        //        yigindi += i;
+        //    }
+        //}
+
+        //Console.WriteLine(yigindi);
+
+        // 15
+
+
+
+        //for ( int i = 100; i <= 999; i++ )
+        //{
+
+        //    var r1 = i % 10;
+        //    var r2 = i / 10 % 10;
+        //    var r3 = i / 100 % 10;
+
+        //    if ( r1 + r2 + r3 > 18 )
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+
+        // 16
+
+        //for ( int i = 100 i <= 999 ; i++ )
+        //{
+
+        //    var r1 = i % 10;
+        //    var r2 = i / 10 % 10;
+        //    var r3 = i / 100 % 10;
+
+        //    if ( r1 == r3 )
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        // 17
+
+        //var counter = 0;
+        //var yigindi = 0;
+
+
+        //for ( int i = 10; i <= 99; i++)
+        //{
+
+        //    var r1 = i % 10;
+        //    var r2 = i / 10 % 10;
+
+        //    if ( r1 *  r2 > 15 )
+        //    {
+        //        counter++;
+        //        yigindi += i;
+        //    }
+
+        //    Console.Write("15 dan katta bo'lgan sonlarni miqdorini = " + counter);
+        //    Console.Write("15 dan katta bo'lgan sonlarni yigindisi = " + yigindi);
+
+
+        //}
+
+
+
+        // 18
+
+        //for ( int i = 100;i <= 999; i++)
+        //{
+
+        //    var r1 = i % 10;
+        //    var r2 = i / 10 % 10;
+        //    var r3 = i / 100 % 10;
+
+        //    if ( r2 == 4 || r2 == 7 )
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //}
+
+
+        // 19
+
+
+        //Console.WriteLine(" a sonini kiriting : ");
+
+        //var intValue = int.Parse(Console.ReadLine());
+
+        //for (int i = 1; i < intValue; i++)
+        //{
+        //    Console.WriteLine(intValue);
+        //}
+
+
+        // 20
+
+        //Console.Write(" a sonini kiriting : "); 
+
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+        //for ( int i = 1 ; i <= secondIntValue ; i++ )
+        //{
+        //    Console.WriteLine(firstIntValue);
+        //}
+
+
+        // 21
+
+
+        //Console.Write(" a sonini kiriting : ");
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+
+        //for ( int i = 1; i <=  firstIntValue; i++ )
+        //{
+        //    Console.WriteLine(secondIntValue);
+        //}
+
+
+        // 22
+
+
+        //Console.Write(" a sonini kiriting : ");
+
+        //var intValue = int.Parse(Console.ReadLine());
+
+        //for ( int i = 1; i <= intValue; i++)
+        //{
+        //    if ( intValue % i == 0 )
+        //    {
+        //        Console.WriteLine(i);
+        //    } 
+        //} 
+
+
+        // 23
+
+        // Console.Write(" a sonini kiriting : ");
+
+        // var intValue = int.Parse(Console.ReadLine());
+
+        // var counter = 0;
+
+        // for ( int i = 1; i <= intValue; i++)
+        // {
+        //     if ( intValue % i == 0)
+        //     {
+        //         counter++;
+        //     }
+        // }
+        //Console.Write( intValue + " ni boluvchilarini miqdori = " + counter);
+
+
+
+
+        // 24
+
+
+        //Console.Write(" a sonini kiriting : ");
+
+        //var intValue = int.Parse(Console.ReadLine());
+
+        //var yigindi = 0;
+
+        //for ( int i = 1; i <= intValue; i++)
+        //{
+        //    if ( intValue % i == 0)
+        //    {
+        //        yigindi += i;
+        //    }
+        //}
+        //Console.Write( intValue + " ni boluvchilarini yigindisi = " + yigindi);
+
+
+        // 25
+
+
+        //Console.Write("Son kiriting : ");
+        //var num = int.Parse(Console.ReadLine()); // 24
+        //var counter = 0;
+
+        //for  (int i = 1; i <= num; i++)
+        //{
+        //    if ( num % i == 0 )
+        //    {
+        //        counter++;
+        //    }
+        //}
+
+
+        //if ( counter == 2 )
+        //{
+        //    Console.Write("true");
+        //}
+        //else
+        //{
+        //    Console.Write("false");
+        //}
+
+
+        // 26
+
+        //Console.Write(" 1 chi soni kiriting : "); 
+
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" 2 chi soni kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+        //var firstcounter = 0;
+        //var secondcounter = 0;
+
+        //for (int i = 1; i <= firstIntValue; i++)
+        //{
+        //    if ( firstIntValue % i == 0 )
+        //    {
+        //        firstcounter++;
+        //    }
+        //}
+
+        //for (int i = 1; i <= secondIntValue; i++)
+        //{
+        //    if ( secondIntValue % i == 0 )
+        //    {
+        //        secondcounter++;
+        //    }
+        //}
+
+        //if ( firstIntValue == 2 && secondcounter == 2 )
+        //{
+        //    Console.Write( firstIntValue + secondIntValue );
+        //}
+        //else
+        //{
+        //    Console.Write( firstIntValue * secondIntValue );
+        //}
+
+
+        // 27
+
+        //var price = 4000;
+
+        //for ( int i = 1; i <= 60; i++ )
+        //{
+        //    var overall =  price * i;
+        //    Console.WriteLine( i + " kg shakar = " + overall + " so'm ");
+        //}
+
+
+        // 28
+
+        //Console.Write( " son kiriting : ");
+        //var num = int.Parse(Console.ReadLine());
+
+        //var counter = 0;
+
+        //for (int i = 35; i <= num; i++)
+        //{
+        //    if ( i % 3 != 0 )
+        //    {
+        //        Console.WriteLine(i);
+        //        counter++;
+        //    }
+        //}
+
+        //Console.Write(" 3 ga bolinmidigan sonlar soni : " + counter);
+
+
+        // 29
+
+        //Console.Write(" a sonini kiriting : ");
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine())
+
+
+        //    var firstcounter = 0;
+        //    var secondcounter = 0;
+
+
+
+        //for ( int i = 1; i<= firstIntValue; i++ )
+        //{
+        //    if ( firstIntValue % i == 0 )
+        //    {
+        //        firstcounter++;
+        //    }
+        //}
+
+        //for ( int i = 1; i<= secondcounter; i++ )
+        //{
+        //    if ( secondIntValue % i == 0 )
+        //    {
+        //        secondcounter++;
+        //    }
+        //}
+
+        //if (firstcounter > secondcounter)
+        //{
+        //    Console.Write(" a sonini boluvchilari koproq ");
+        //}
+
+        //else if (secondcounter > firstcounter)
+        //{
+        //    Console.Write(" b sonini boluvchilari koproq ");
+        //}
+
+        //else
+        //{
+        //    Console.Write("a va b sonlarining bo'luvchilari teng");
+        //}
+
+
+
+        // 30
+
+
+        //Console.Write(" a sonini kiriting : ");
+        //var firstIntValue = int.Parse(Console.ReadLine());
+
+        //Console.Write(" b sonini kiriting : ");
+
+        //var secondIntValue = int.Parse(Console.ReadLine());
+
+
+        //var yigindi = 0;
+
+
+        //for ( int i = firstIntValue + 1 ; i < secondIntValue; i++ )
+        //{
+        //    yigindi += i;
+        //}
+
+        //Console.Write( " yigindi = " + yigindi);
+
+
+        // 31
+
+
+
+
+
+
+
+        // 32
+
+
+
+
+
+        // 33
+
+
+        Console.Write("n sonini kiriting: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("k sonini kiriting: ");
+        int k = int.Parse(Console.ReadLine());
+
+        var result = 1;
+
+        for ( int i = 1; i <= k; i++ )
+        {
+            result *= n;
+        }
+
+        Console.Write( " Natija : " + result);
+
+
+
+
+        //                 FAYL 1.3.2
+
+
+
 
 
     }
