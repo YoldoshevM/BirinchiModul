a4390e3434769c3e7f248841e9823720e4d794c9
YoldoshevM <yoldoshevm396@gmail.com>
2025-12-12 23:13:01 +0500

    6 dars homework full

---
+++ Dars6/Dars6/Dars6/Program.cs
@@ -0,0 +1,303 @@
+﻿namespace Dars6;
+
+internal class Program
+{
+
+
+    // 1
+
+
+
+    //static float Add (float a , float b ) {
+
+
+    //    return a + b;
+
+    //}
+    //static float Add (float a , float b , float c ) {
+
+
+    //    return a + b + c;
+
+    //}
+
+    //static string Add ( string s1 , string s2 ) {
+
+
+    //    return s1 + s2;
+
+    //}
+
+
+
+    static void Main(string[] args)
+    {
+
+        // 1
+
+        //Console.WriteLine(Add( 1.4f , 3f) );
+        //Console.WriteLine(Add( 1.4f , 1.5f , 2f));
+        //Console.WriteLine(Add( "4" , "1") );
+
+
+        // 3
+
+
+        //Console.WriteLine(Calculate(4, 5));
+        //Console.WriteLine(Calculate(4, 5, 2));
+
+
+        // 4
+
+        //Console.Write(getAmountDigit(42));
+        //Console.WriteLine(getAmountDigit("a12b34d5"));
+
+        // 5
+
+        //Console.WriteLine( findMax(2 , 3 , 5));
+        //Console.WriteLine( findMax(2 , 3 , 5 , 8));
+        //Console.WriteLine( findMax(2 , 3 , 5 , 8 , 19));
+
+        // 6
+
+        //Console.WriteLine(multiplication(4));
+        //Console.WriteLine(multiplication(4 , 5));
+        //Console.WriteLine(multiplication(4 , 5 ,2));
+
+
+
+
+
+        // 7
+
+
+        //Console.Write( " Son kiriting : ");
+
+        //var a = int .Parse( Console.ReadLine() );
+
+        //Console.Write( " Xonalar soni : " + CountDigit(a));
+
+
+
+
+        // 8
+
+        //Console.Write("a ni kiriting: ");
+        //int a = int.Parse(Console.ReadLine());
+
+        //Console.Write("b ni kiriting: ");
+        //int b = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine(SumAB(a , b));
+
+
+
+
+
+
+        // 2
+
+
+
+
+
+
+        // 3
+
+
+        //static int calculate(int a, int b)
+        //{
+
+        //    return a + b;
+
+        //}
+
+        //static int calculate(int a, int b, int c)
+        //{
+
+        //    return a * b * c;
+
+        //}
+
+
+
+
+        // 4
+
+
+
+
+        //static int getAmountDigit(int a)
+        //{
+        //    return a.ToString().Length;
+        //}
+
+
+        //static int getAmountDigit(string s)
+        //{
+        //    int count = 0;
+
+        //    foreach (char c in s)
+        //    {
+        //        if (char.IsDigit(c))
+        //        {
+        //            count++;
+        //        }
+        //    }
+
+        //    return count;
+        //}
+
+
+
+        // 5
+
+
+        //static int findMax(int a , int b , int c )
+        //{
+
+        //    return Math.Max(a, Math.Max(c, b));
+
+
+        //}
+
+        //static int findMax(int a , int b , int c , int d )
+        //{
+
+        //    return Math.Max(Math.Max(a,b), Math.Max(c, d));
+
+
+        //}
+
+        //static int findMax(int a , int b , int c , int d , int e )
+        //{
+
+
+        //    int max1 = Math.Max(a, b);
+        //    int max2 = Math.Max(c ,d);
+        //    int max3 = Math.Max(max1, max2);
+        //    int max4 = Math.Max(max3, e);
+
+
+        //    return max4;
+
+
+        //}
+
+
+
+
+        // 6
+
+
+        //static int multiplication( int a )
+        //{
+
+        //    var result = 1;
+
+        //    for ( int i = 0; i <= a; i++ )
+        //    {
+
+        //        result *= i;
+
+        //    }
+
+        //    return result;
+
+        //}
+
+        //static int multiplication( int a , int b )
+        //{
+
+        //    var result = 1;
+
+        //    for ( int i = a; i <= b; i++ )
+        //    {
+
+        //        result *= i;
+
+        //    }
+
+        //    return result;
+
+        //}
+
+        //static int multiplication( int a , int b , int c )
+        //{
+
+
+        //    return a * b * c;
+
+        //}
+
+
+
+        // 7
+
+
+        //static int CountDigit( int n )
+        //{
+
+        //    if ( n == 0 )
+        //    {
+        //        return 0;
+        //    }
+
+
+        //    return 1 + CountDigit(n / 10);
+
+
+
+        //}
+
+
+
+        // 8
+
+
+
+        //static int SumAB(int a, int b)
+        //{
+
+        //    if (a < b)
+        //    {
+        //        return 0;
+        //    }
+
+
+        //    return a + SumAB(a + 1, b);
+
+
+
+        //}
+
+
+
+        // 9
+
+
+
+        //Console.Write(" Soni kiriting : "); 
+
+        //var n = int.Parse(Console.ReadLine());
+
+
+        //var sum = 0;
+
+        //for ( int i = 0; i <= n; i++ )
+        //{
+
+        //    sum += i * i * i; 
+
+        //}
+
+        //Console.Write(" Sonlar kubi yigindisi : " + sum);
+
+
+
+
+
+    }
+
+}
+
