eb95616179322a27cc6a03d07fdf2fce430af66c
YoldoshevM <yoldoshevm396@gmail.com>
2026-02-26 12:20:26 +0500

    previus lessons

---
+++ 3.7 Dars/src/3.7 Dars/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _3._7_Dars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 1
+
+
+            //var func = (int a, int b, int c) =>
+            //{
+            //    return a + b + c;
+            //};
+
+
+            //  2
+
+
+            var action = (string s1, string s2) => s1 + s2;
+          
+            Console.WriteLine("salo" + "m");
+
+
+            // 3
+
+
+
+
+
+
+            //// 4
+
+
+            //Action<int> ints = (int book) =>
+            //{
+
+            //    var onBarobar = book * 10;
+
+            //    Console.WriteLine(onBarobar);
+
+            //};
+
+
+        }
+    }
+}
