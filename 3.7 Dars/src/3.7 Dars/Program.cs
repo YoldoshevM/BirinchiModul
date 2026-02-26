04e769f27108134f2eeba38f61d05d8db9040b26
YoldoshevM <yoldoshevm396@gmail.com>
2026-02-26 13:06:11 +0500

    3.7 dars uy ishi

--- 3.7 Dars/src/3.7 Dars/Program.cs
+++ 3.7 Dars/src/3.7 Dars/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace _3._7_Dars
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Intrinsics.Arm;
+
+namespace _3._7_Dars
 {
     internal class Program
     {
@@ -8,40 +11,188 @@
             // 1
 
 
-            //var func = (int a, int b, int c) =>
+            //var f1 = (User user) =>
             //{
-            //    return a + b + c;
+
+            //    if (user.Age >= 18 )
+            //    {
+            //        return "Adult";
+            //    }
+
+
+            //    return "Minor";
+
+
             //};
 
 
-            //  2
+
+            // 2
 
 
-            var action = (string s1, string s2) => s1 + s2;
-          
-            Console.WriteLine("salo" + "m");
+            //var act1 = (Car car) =>
+            //{
+
+            //    Console.WriteLine(DateTime.Now.Year - car.Year);
+
+            //};
 
 
             // 3
 
 
-
-
-
-
-            //// 4
-
-
-            //Action<int> ints = (int book) =>
+            //var f2 = (List<Product> products) =>
             //{
 
-            //    var onBarobar = book * 10;
+            //    return products.OrderBy(p => p.Price).First();
 
-            //    Console.WriteLine(onBarobar);
 
             //};
+
+
+            // 4
+
+
+            //var act2 = (Order order) =>
+            //{
+
+
+            //    Console.WriteLine(order.Price * order.Quantity);
+
+
+            //};
+
+
+            // 5
+
+
+            //var f3 = (List<Student> students) =>
+            //{
+
+
+            //    return students.Where(s => s.Grade > 90).ToList;
+
+
+            //};
+
+
+
+            // 6
+
+
+            //var f4 = (Employee employee) =>
+            //{
+
+            //    var newS =  employee.Salary * 1.15m;
+
+            //    return newS;
+
+
+            //};
+
+
+
+            // 7
+
+
+            //var Unli = (string s ) =>
+            //{
+
+
+            //    return s.Count( s => "aeiou".Contains( s ) );
+
+
+            //};
+
+
+
+            // 8
+
+
+            //var act3 = (int a , int b) =>
+            //{
+
+            //    Console.WriteLine(Math.Max(a,b));
+
+            //};
+
+
+
+            // 9
+
+
+            //var f5 = (int n) =>
+            //{
+
+            //    if ( n % 2 != 0 )
+            //    {
+            //        return true;
+            //    }
+
+            //    return false;
+
+
+            //};
+
+
+
+            // 10
+
+
+            //var f6 = (string s) =>
+            //{
+
+
+            //    return new string (s.Reverse().ToArray());
+
+
+            //};
+
 
 
         }
     }
 }
+
+//public class User
+//{
+
+//    public string Name { get; set; }
+//    public int Age { get; set; }
+
+//}
+//public class Car
+//{
+
+//    public string Model { get; set; }
+//    public int Year { get; set; }
+
+//}
+//public class Product
+//{
+
+//    public string Name { get; set; }
+//    public decimal Price { get; set; }
+
+//}
+//public class Order
+//{
+
+//    public int Quantity { get; set; }
+//    public decimal Price { get; set; }
+
+//}
+//public class Student
+//{
+
+//    public string Name { get; set; }
+//    public decimal Grade { get; set; }
+
+//}
+//public class Employee
+//{
+
+//    public string Name { get; set; }
+//    public decimal Salary { get; set; }
+
+//}
