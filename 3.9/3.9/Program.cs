39ab1fc4a2605bdd7fa2debd8c23c0190b64482a
YoldoshevM <yoldoshevm396@gmail.com>
2026-03-03 11:49:11 +0500

    3.9 uy ishi

--- 3.9/3.9/Program.cs
+++ 3.9/3.9/Program.cs
@@ -124,15 +124,215 @@ internal class Program
 
         // 10
 
-        list.Add(1);
-        list.Add(1);
-        list.Add(1);
+        //list.Add(1);
+        //list.Add(23);
+        //list.Add(1125);
 
-        for ( int i = 0; i < list.Count; i++ )
-        {
-            Console.WriteLine(list[i]);
-        }
+        //for ( int i = 0; i < list.Count; i++ )
+        //{
+        //    Console.WriteLine(list[i]);
+        //}
 
+
+
+        // 11
+
+        //var sum = 0;
+
+
+        //foreach ( var num in list )
+        //{
+        //    sum += (int)num;
+        //}
+        //Console.WriteLine(sum);
+
+
+
+        // 12
+
+
+        //var intmax = (int)list[0];
+
+
+        //foreach ( var num in list )
+        //{
+        //    if ( (int)num > intmax )
+        //    {
+        //        intmax = (int)num;
+        //    }
+        //}
+
+        //Console.WriteLine(intmax);
+
+
+
+
+
+        // 21
+
+        LinkedList<int> linked = new LinkedList<int>();
+
+        //linked.AddLast(1);
+        //linked.AddLast(2);
+        //linked.AddLast(3);
+        //linked.AddLast(4);
+        //linked.AddLast(5);
+
+        //foreach(int i in linked)
+        //{
+        //    Console.WriteLine(i);
+        //}
+
+
+
+        // 22
+
+        //foreach (var item in linked)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+
+        // 23
+
+
+        //linked.AddFirst(902);
+
+
+
+        // 24
+
+
+        //linked.AddLast(902);
+
+
+
+
+        // 25
+
+
+        //linked.RemoveFirst();
+
+
+
+
+        // 26
+
+
+        //linked.RemoveLast();
+
+
+
+
+        // 28
+
+
+        //Console.WriteLine(linked.Contains(2));
+
+
+
+
+        // 29
+
+
+        //Console.WriteLine(linked.Reverse);
+
+
+
+        // 30
+
+
+
+
+
+        // 31
+
+
+        //linked.AddLast(1);
+        //linked.AddLast(2);
+
+        //var sum = linked.Sum(i => i);
+
+        //Console.WriteLine(sum);
+
+
+
+
+        // 32
+
+
+        //linked.AddLast(1);
+        //linked.AddLast(2);
+
+        //var max = linked.Max(i => i);
+
+        //Console.WriteLine(max);
+
+
+
+
+        // 33
+
+
+
+        //linked.AddLast(1);
+        //linked.AddLast(2);
+
+        //var min = linked.Min(i => i);
+
+        //Console.WriteLine(min);
+
+
+
+
+
+        // 34
+
+
+        //for (int i = 1; i <= 20 ; i++)
+        //{
+        //    linked.AddLast(i);
+        //}
+
+        //foreach (int i in linked)
+        //{
+        //    if ( i % 2 != 0 )
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+
+
+
+        // 35
+
+
+        //LinkedList<int> linked1= new LinkedList<int>();
+
+        //linked1.AddLast(1);
+        //linked1.AddLast(3);
+        //linked1.AddLast(4);
+        //linked1.AddLast(5);
+
+
+        //LinkedList<int> linked2 = new LinkedList<int>();
+
+
+        //foreach ( var item  in linked1 )
+        //{
+        //    linked2.AddLast(item);
+        //}
+
+
+        //foreach (int item in linked2)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+
+
+
+        // 36
 
 
 
